3fb153c43b6ac9d34d73e7d6061f61d4e15c8645
KXAND <71508678+KXAND@users.noreply.github.com>
2026-07-07 20:26:16 +0800

    feat(sim): add plant id constants

---
+++ simenv/consts.py
@@ -0,0 +1,12 @@
+# plants id
+class Plants:
+    Sunflower = 1
+    Peashooter = 0
+    Potato_Mine = 4
+    Wallnut = 3
+    Repeater = 7
+    Squash = 17
+    Cherry_Bomb = 2
+    Snow_Pea = 5
+    Melon_pult = 39
+    Spikeweed = 21
